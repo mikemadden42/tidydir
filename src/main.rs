87a233df65bfa5a7e1bcfb0271deb6b492212742
Joe Miner <joe.miner@mst.edu>
2025-03-23 06:38:28 -0500

    Fix some formatting issues.

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,13 @@ fn clean() -> io::Result<()> {
             let file_name = entry.file_name();
             let file_name_str = file_name.to_string_lossy();
 
-            // Skip dotfiles
+            // Skip dot files
             if !file_name_str.starts_with('.') {
                 if let Some(extension) = path.extension() {
                     if let Some(ext_str) = extension.to_str() {
                         if ext_str != "9" {
                             let dest_dir = Path::new("Documents").join(ext_str);
-
                             fs::create_dir_all(&dest_dir)?;
-
                             let dest_path = dest_dir.join(&file_name);
                             if dest_path.exists() {
                                 println!("File {path:?} already exists in {dest_dir:?}");
